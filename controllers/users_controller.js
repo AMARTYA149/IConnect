2c159006d2dd3a3f7d0d3e5802d089a382bd6db3
Amartya Aishwarya <amartya970914@gmail.com>
2021-01-12 23:45:32 +0530

    signIn & signUp form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,3 +3,27 @@ module.exports.profile = function (req, res) {
     title: "User Title",
   });
 };
+
+//render sign up page
+module.exports.signUp = function (req, res) {
+  return res.render("user_sign_up", {
+    title: "IConnect | Sign Up",
+  });
+};
+
+//render sign in page
+module.exports.signIn = function (req, res) {
+  return res.render("user_sign_in", {
+    title: "IConnect | Sign In",
+  });
+};
+
+//get the sign up data
+module.exports.create = function (req, res) {
+  //TODO later
+};
+
+//sign in and create session for the user
+module.exports.createSession = function (req, res) {
+  //TODO later
+};
