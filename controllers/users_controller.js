2e921de7dd5015accc9549fc13a1d0a5c40d7134
Amartya Aishwarya <amartya970914@gmail.com>
2021-01-30 12:21:35 +0530

    showing all user profile links

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,8 +1,11 @@
 const User = require("../models/user");
 
 module.exports.profile = function (req, res) {
-  return res.render("user_profile", {
-    title: "User Title",
+  User.findById(req.params.id, function (err, user) {
+    return res.render("user_profile", {
+      title: "User Title",
+      profile_user: user,
+    });
   });
 };
 
