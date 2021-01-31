87537f1e981f625a190c490eb791088f850e95ff
Amartya Aishwarya <amartya970914@gmail.com>
2021-01-31 20:05:26 +0530

    noty notifications for posts

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -11,10 +11,11 @@ module.exports.create = async function (req, res) {
       content: req.body.content,
       user: req.user._id,
     });
+    req.flash("success", "Post published!");
     return res.redirect("back");
   } catch (error) {
-    console.log("Error", error);
-    return;
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
 
@@ -26,12 +27,15 @@ module.exports.destroy = async function (req, res) {
       post.remove();
 
       await Comment.deleteMany({ post: req.params.id });
+      req.flash("success", "Post and associated comments deleted!");
+
       return res.redirect("back");
     } else {
+      req.flash("error", "You can't delete this post'!");
       return res.redirect("back");
     }
   } catch (error) {
-    console.log("Error", error);
-    return;
+    req.flash("error", err);
+    return res.redirect("back");
   }
 };
