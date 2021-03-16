1c120c400efa95aff70f855391e6aad366d11237
Amartya Aishwarya <amartya970914@gmail.com>
2021-03-16 22:24:11 +0530

    production setup

--- config/environment.js
+++ config/environment.js
@@ -22,6 +22,26 @@ const development = {
 
 const production = {
   name: "production",
+  asset_path: process.env.ICONNECT_ASSET_PATH,
+  session_cookie_key: process.env.ICONNECT_SESSION_COOKIE_KEY,
+  db: process.env.ICONNECT_DB,
+  smtp: {
+    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.ICONNECT_GMAIL_USERNAME,
+      pass: process.env.ICONNECT_GMAIL_PASS,
+    },
+  },
+  google_client_id: process.env.ICONNECT_GOOGLE_CLIENT_ID,
+  google_client_secret: process.env.ICONNECT_GOOGLE_CLIENT_SECRET,
+  google_callback_url: process.env.ICONNECT_GOOGLE_CLIENT_URL,
+  jwt_secret: process.env.ICONNECT_JWT_SECRET,
 };
 
-module.exports = development;
+module.exports =
+  eval(process.env.ICONNECT_ENVIRONMENT) == undefined
+    ? development
+    : eval(process.env.ICONNECT_ENVIRONMENT);
